7cc5cacf0ac5652ca44c66f324265a5770ddc058
Sidhartha.Verma <sidharthv91@gmail.com>
2024-03-22 15:42:26 -0300

    feat: netlify deploy trial

---
+++ helpers/apiKeys.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwKbz6Wo1UVp1BzKSJVXV6hTz3vwy74k8",
+    authDomain: "netflixgpt-57a56.firebaseapp.com",
+    projectId: "netflixgpt-57a56",
+    storageBucket: "netflixgpt-57a56.appspot.com",
+    messagingSenderId: "1093133802935",
+    appId: "1:1093133802935:web:c284961959cce2a54adc32",
+    measurementId: "G-DEHFXGZ999",
+};
+
+export default firebaseConfig;
