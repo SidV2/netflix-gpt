4faa4d3de7b32e9e5065f91c6395921ab780c458
Sidhartha.Verma <sidharthv91@gmail.com>
2024-03-22 15:50:36 -0300

    feat: netlify deploy test

--- helpers/apiKeys.js
+++ helpers/apiKeys.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAwKbz6Wo1UVp1BzKSJVXV6hTz3vwy74k8",
-    authDomain: "netflixgpt-57a56.firebaseapp.com",
-    projectId: "netflixgpt-57a56",
-    storageBucket: "netflixgpt-57a56.appspot.com",
-    messagingSenderId: "1093133802935",
-    appId: "1:1093133802935:web:c284961959cce2a54adc32",
-    measurementId: "G-DEHFXGZ999",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
