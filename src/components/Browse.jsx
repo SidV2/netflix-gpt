d49720b1b30a7ca03d1b4efaa9e593d6519d95cb
Sidhartha.Verma <sidharthv91@gmail.com>
2024-04-07 21:07:57 -0300

    feat: open ai apis and query intergated

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,25 +1,29 @@
-
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
-import useTopRatedMovies from "../hooks/useTopRatedMovies";
-import useUpComingMovies from "../hooks/useUpComingMovies";
+import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
-    useNowPlayingMovies();
-    usePopularMovies();
-    useTopRatedMovies();
-    useUpComingMovies();
-    
-    return (
+  useNowPlayingMovies();
+  usePopularMovies();
+
+  return (
+    <div>
+      <Header />
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
         <>
-            <Header hideYouTubeControls={true} />
-            <MainContainer />
-            <SecondaryContainer />
+          <MainContainer />
+          <SecondaryContainer />
         </>
-    )
+      )}
+    </div>
+  );
 };
 export default Browse;
--- src/components/GptMovieSuggestions.js
+++
@@ -1,2 +0,0 @@
-const GptMovieSuggestions = () => {}
-export default GptMovieSuggestions
